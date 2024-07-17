0e57bcc6520f63397b65731e128fceeba8cbd53a
Harshit Kumar <kharshti020@gmail.com>
2024-07-17 18:42:53 +0530

    Added Weather-Info card

--- src/app/UI.js
+++ src/app/UI.js
@@ -16,6 +16,7 @@ export class UI{
     }
 
     static updateWeatherCard(data){
+      if(data != undefined){
       const info = data.currentConditions
       const date = new Date()
       
@@ -26,6 +27,26 @@ export class UI{
       document.querySelector(".card-title").textContent = data.address
       document.querySelector("#date-today").textContent = date.getDate() + " " + UI.getMonthName(date.getMonth())
       
+      UI.updateWeatherInfoCard(data)
+      UI.generateWeatherForeCast(data)
+      }
+
+    }
+
+    static updateWeatherInfoCard(data){
+
+      const info = data.days[0]
+
+      document.querySelector("#high").textContent = `${info.tempmax}°`
+      document.querySelector("#low").textContent = `${info.tempmin}°`
+      document.querySelector("#sunrise").textContent = info.sunrise.substring(0,info.sunrise.length - 3)
+      document.querySelector("#sunset").textContent = info.sunset.substring(0,info.sunset.length - 3)
+      document.querySelector("#feels").textContent = `${info.feelslike}°`
+      document.querySelector("#uv").textContent = info.uvindex
+      document.querySelector("#rain").textContent = `${info.precipprob}%`
+      document.querySelector("#visibility").textContent = info.visibility
+      document.querySelector(".weather-description").textContent = data.description
+
 
     }
 
@@ -35,6 +56,22 @@ export class UI{
         return arr[month-1];
 
     }
+
+    static generateWeatherForeCast(data){
+       
+      const container = document.querySelector(".weather-forecast")
+
+      data.days.forEach((day)=>{
+         const dayCard = document.createElement("div")
+         dayCard.classList.add("day-card")
+
+         const svg = document.createElement("div")
+                                                                                                                                                                                                                                                                                                                                          
+
+         container.append(dayCard)
+      })
+
+    } 
 
     static getLocData(){
 
